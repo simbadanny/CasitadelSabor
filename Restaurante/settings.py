6d4b63a42de9e58693bed3a4b08390c28a547f82
simbadanny <dannysimba7@gmail.com>
2024-08-14 14:48:05 -0500

    update_config

--- Restaurante/settings.py
+++ Restaurante/settings.py
@@ -142,8 +142,10 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Restaurante/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Restaurante/media')
+MEDIA_ROOT = os.path.join('/mnt/disco_persistente', 'Restaurante/media')
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'

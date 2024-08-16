f9971d12c19322a7016064d34c9c3babe84ce457
simbadanny <dannysimba7@gmail.com>
2024-08-16 08:38:23 -0500

    update89

---
+++ Restaurante/backends.py
@@ -0,0 +1,20 @@
+# myapp/backends.py
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+
+class EmailOrUsernameModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        if '@' in username:
+            try:
+                user = User.objects.get(email=username)
+            except User.DoesNotExist:
+                return None
+        else:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return None
+
+        if user.check_password(password):
+            return user
+        return None
